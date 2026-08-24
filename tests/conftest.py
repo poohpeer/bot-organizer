51cdc9ef4744d0c1aae782014a78d52a330236f4
Alex <poohpeer@gmail.com>
2026-08-24 11:47:21 +0300

    Bootstrap project skeleton (pyproject.toml, package layout, test env defaults)

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault("BOT_TOKEN", "123456:test-token")
+os.environ.setdefault("GEMINI_API_KEY", "test-key")
+os.environ.setdefault("GOOGLE_MAPS_API_KEY", "test-maps-key")
